661d30296168351540409ff0ae2c87cc948d0263
Fabio Silva <fabiovariant@hotmail.com>
2018-05-03 01:01:12 -0300

    Adicionando bootstrap. Adicionando genérico de quadro de jobs

---
+++ src/static/src/views/JobCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-card :title="jobName"
+          img-src="https://lorempixel.com/600/300/food/5/"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2">
+    <p class="card-text">
+      {{jobDesc}}
+    </p>
+    <b-button :href=jobExecCall variant="primary">Executar</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'jobCard',
+  props: ['jobName', 'jobDesc', 'jobExecCall']
+}
+</script>
